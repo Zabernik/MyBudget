285a200f831a94f993fa757db65ef2a3b46e1dbf
Wiktor Pacewicz <wiktor.pacewicz@microsoft.wsei.edu.pl>
2024-09-10 20:57:01 +0200

    Update v0.1 Initial setup and layout

---
+++ MyBudget/Controllers/IncomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBudget.Controllers
+{
+    public class IncomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyBudget/Controllers/MetalsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBudget.Controllers
+{
+    public class MetalsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
