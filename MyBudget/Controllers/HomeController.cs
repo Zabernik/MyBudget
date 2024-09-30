2f17686bb5da8235d3362794afbe10e744120c21
Wiktor Pacewicz <wiktor.pacewicz@microsoft.wsei.edu.pl>
2024-10-01 01:38:21 +0200

    Update v0.4 Start creating dashboard for deposits

--- MyBudget/Controllers/HomeController.cs
+++ MyBudget/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using MyBudget.Data; // Namespace twojego kontekstu bazy danych
 using MyBudget.Models;
 using System.Diagnostics;
+using System.Linq;
 
 namespace MyBudget.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MyBudgetContext _context; // Dodaj kontekst bazy danych
 
-        public HomeController(ILogger<HomeController> logger)
+        // Zmodyfikowany konstruktor przyjmuj¹cy równie¿ kontekst bazy danych
+        public HomeController(ILogger<HomeController> logger, MyBudgetContext context)
         {
             _logger = logger;
+            _context = context; // Inicjalizujemy kontekst
         }
 
+        // Pobierz listê depozytów z bazy danych
         public IActionResult Index()
         {
-            return View();
+            // Pobieramy listê depozytów z bazy danych za pomoc¹ Entity Framework
+            var deposits = _context.Deposit.ToList();
+
+            // Przekazujemy listê depozytów do widoku
+            return View(deposits);
         }
 
         public IActionResult Privacy()
